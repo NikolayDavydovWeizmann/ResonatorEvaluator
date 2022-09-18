31b4f2a04d8cdf329774168e2ca29b31aa813517
Nikolay Davydov <nikolay.davydov@weizmann.ac.il>
2022-09-18 17:49:14 +0300

    Realign func corrected. Working for 2 mirrors.

--- evaluator.py
+++ evaluator.py
@@ -192,30 +192,40 @@ class System:
     def realign(self):
         if self.is_consistent():
             return
-        r_transform_mx = np.matrix([[1, 1], [0, 1]])
+
+        r_transform_mx = np.matrix([[1, 0], [0, 1]])
         for i in range(1, self.num_of_mirrors - 1):
             r_transform_mx = np.matmul(np.matrix([[1, self.elems[i].coord - self.elems[i - 1].coord], [0, 1]]), r_transform_mx)
             r_transform_mx = np.matmul(self.elems[i].get_matrix_tangential(), r_transform_mx)
             r_transform_mx = np.matmul(self.elems[i].get_matrix_tangential(), r_transform_mx)
         r_transform_mx = np.matmul(np.matrix([[1, (self.elems[-1].coord - self.elems[-2].coord)], [0, 1]]), r_transform_mx)
+
         z_central_start = self.elems[0].radius * np.cos(np.pi / 180 * self.elems[0].angle)
         x_central_start = self.elems[0].radius * np.sin(np.pi / 180 * self.elems[0].angle)
-        term_radius = self.elems[-1].radius
+
+        self.elems[0].angle = 0
+        self.elems[0].coord = 0
+
+        term_radius = -1 * self.elems[-1].radius
+
         x_central_term = self.elems[-1].radius * np.sin(np.pi / 180 * self.elems[-1].angle) / (r_transform_mx[1, 0] * term_radius + r_transform_mx[0, 0])
         new_radius = (r_transform_mx[1, 1] * term_radius + r_transform_mx[0, 1]) / (r_transform_mx[1, 0] * term_radius + r_transform_mx[0, 0])
         z_central_term = np.sqrt(new_radius ** 2 - x_central_term ** 2)
-        direct = np.array([np.abs(z_central_term - z_central_start), x_central_term - x_central_start])
+
+        direct = np.array([np.abs(z_central_term - z_central_start), x_central_start - x_central_term])
         direct = 1 / np.sqrt(direct[0] ** 2 + direct[1] ** 2) * direct
         opt_path = self.elems[0].radius
         start_coord = np.array([z_central_start, x_central_start])
+
         for i in range(1, self.num_of_mirrors):
             central_coord = self.elems[i].get_central_coord()
+
             if np.abs(direct[1]) < 0.000000001:
                 x_solutions = np.array([start_coord[1], start_coord[1]])
                 if np.abs(x_solutions[0] - central_coord[1]) > self.elems[i].radius:
                     raise Exception("Missing mirror")
                 z_diff = np.sqrt(self.elems[i].radius ** 2 - (x_solutions[0] - central_coord[1]) ** 2)
-                z_solutions = np.array([central_coord[0] + z_diff, central_coord[0] + z_diff])
+                z_solutions = np.array([central_coord[0] + z_diff, central_coord[0] - z_diff])
             else:
                 A_coef = 1
                 B_coef = 2 * (direct[0] * direct[1] * (start_coord[0] - central_coord[0]) - direct[0] ** 2 * start_coord[1] - direct[1] ** 2 * central_coord[1])
@@ -225,9 +235,13 @@ class System:
                 if np.isnan(x_solutions[0]):
                     raise Exception("Missing mirror")
                 z_solutions = start_coord[0] * np.array([1, 1]) + direct[0] / direct[1] * (x_solutions - start_coord[1] * np.array([1, 1]))
+            
             cntrl_direct = np.array([[central_coord[0] - z_solutions[0], central_coord[1] - x_solutions[0]], [central_coord[0] - z_solutions[1], central_coord[1] - x_solutions[1]]])
             cntrl_direct[0] = cntrl_direct[0] / np.sqrt(cntrl_direct[0, 0] ** 2 + cntrl_direct[0, 1] ** 2)
             cntrl_direct[1] = cntrl_direct[1] / np.sqrt(cntrl_direct[1, 0] ** 2 + cntrl_direct[1, 1] ** 2)
+
+            angle = 0
+
             if direct[0] * cntrl_direct[0, 0] + direct[1] * cntrl_direct[0, 1] < 0:
                 new_start_coord = np.array([z_solutions[0], x_solutions[0]])
                 angle = 180 / np.pi * np.arctan2(direct[1] * cntrl_direct[0, 0] - direct[0] * cntrl_direct[0, 1], -1 * direct[0] * cntrl_direct[0, 0] - direct[1] * cntrl_direct[0, 1])
@@ -236,22 +250,31 @@ class System:
                 new_start_coord = np.array([z_solutions[1], x_solutions[1]])
                 angle = 180 / np.pi * np.arctan2(direct[1] * cntrl_direct[1, 0] - direct[0] * cntrl_direct[1, 1], -1 * direct[0] * cntrl_direct[1, 0] - direct[1] * cntrl_direct[1, 1])
                 direct = direct - 2 * (direct[0] * cntrl_direct[1, 0] + direct[1] * cntrl_direct[1, 1]) * cntrl_direct[1]
+
             d_start_coord = new_start_coord - start_coord
             opt_path += np.sqrt(d_start_coord[0] ** 2 + d_start_coord[1] ** 2)
             start_coord = new_start_coord
             self.elems[i].coord = opt_path
             self.elems[i].angle = angle
+        self.elems[-1].angle = 0
 
             
 
             
 
-m1 = Mirror(0, 10, 0.005)
-m2 = Mirror(0.009, 0, 0.005)
+m1 = Mirror(0, 3, 0.005)
+m2 = Mirror(0.008, 0, 0.005)
 
 
 Sys = System(2, m1, m2)
 
-print(Sys.is_consistent())
 Sys.system_scheme()
+print(Sys.is_consistent())
+print(Sys.get_length())
+
+Sys.realign()
+Sys.system_scheme()
+print(Sys.is_consistent())
+print(Sys.get_length())
+print(Sys.elems[-1].angle)
 
