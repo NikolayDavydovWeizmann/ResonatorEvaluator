d44ffeacdf28f8db0aac18ab4c71fcc6ee298620
Nikolay Davydov <nikolay.davydov@weizmann.ac.il>
2022-10-18 22:47:57 +0300

    Analitical plots

--- analitical_plot.py
+++ analitical_plot.py
@@ -1,15 +1,13 @@
-from math import nan
-import resonator as rs
 import numpy as np
 import matplotlib.pyplot as plt
 
 FUND_WAVE = 1064 * 10 ** -7
 
-NA = 0.1
+NA = 0.05
 z_R = FUND_WAVE / np.pi / NA ** 2
 
 l_1 = 1
-L_3 = np.arange(2, 1000, 2)
+L_3 = np.arange(2, 100, 2)
 
 
 f = (1 + z_R ** 2) / (1 + ((1 + z_R ** 2) / L_3) + np.sqrt(((1 + z_R ** 2) / L_3)) ** 2 - z_R ** 2)
@@ -26,8 +24,8 @@ r_3 = L_3 / 2 + z_R_1 ** 2 * 2 / L_3
 acc = (f * xi_2 - xi_1 * (xi_2 - f)) / (r_1 * (xi_2 - f))
 acc3 = (f * xi_2 - xi_1 * (xi_2 - f)) / r_3 / f
 
-tmp_res = rs.Resonator(2, rs.Mirror(0, 10 ** -13, r_1), rs.Mirror(2, 0, r_1))
-acc_2mirror = 10 ** -13 / tmp_res.realign()
+
+acc_2mirror = 2 * (1 - 1 / r_1)
 
 plt.figure(1)
 plt.plot(L_3 / 2, acc / acc_2mirror)
--- analitical_plot2.py
+++ analitical_plot2.py
@@ -1,25 +1,40 @@
-from math import nan
+from decimal import *
 import resonator as rs
 import numpy as np
 import matplotlib.pyplot as plt
 
-FUND_WAVE = 1064 * 10 ** -7
+C = getcontext()
+C.prec = 28
+PI = Decimal('3.141592653589793238462643383279')
 
-ANGLE = 15 / 180 * np.pi
+FUND_WAVE = Decimal('1064') * C.power(Decimal('10'), Decimal('-7'))
 
-NA = 0.2
-z_R = FUND_WAVE / np.pi / NA ** 2
+"""
+ANGLE = Decimal('15') / Decimal('180') * PI
+"""
 
-l_1 = 1
-L_3 = 100
+NA = Decimal('0.2')
+z_R = FUND_WAVE / PI / C.power(NA, Decimal('2'))
 
-f = (1 + z_R ** 2) / (1 + ((1 + z_R ** 2) / L_3) + np.sqrt(((1 + z_R ** 2) / L_3)) ** 2 - z_R ** 2)
-z_R_1 = z_R * (1 + z_R ** 2) / (((1 + z_R ** 2) / f - 1) ** 2 + z_R ** 2)
+l_1 = Decimal('1')
+L_3 = np.arange(Decimal('2'), Decimal('1000'), Decimal('2'))
 
-r_1 = l_1 + z_R ** 2 / l_1
-r_2 = 2 * f
-r_3 = L_3 / 2 + z_R_1 ** 2 * 2 / L_3
+f = (Decimal('1') + z_R ** Decimal('2')) / (Decimal('1') + ((Decimal('1') + z_R ** Decimal('2')) / L_3) + np.sqrt(((Decimal('1') + z_R ** Decimal('2')) / L_3)) ** Decimal('2') - z_R ** Decimal('2'))
+z_R_1 = z_R * (Decimal('1') + z_R ** Decimal('2')) / (((Decimal('1') + z_R ** Decimal('2')) / f - Decimal('1')) ** Decimal('2') + z_R ** Decimal('2'))
 
-res1 = rs.Resonator(3, rs.Mirror(0, 0, r_1), rs.Mirror(2 * l_1, 0, r_2), rs.Mirror(2 * l_1 + L_3, 0, r_3))
-res1.waist_scheme(FUND_WAVE)
+r_1 = l_1 + z_R ** Decimal('2') / l_1
+r_2 = Decimal('2') * f
+r_3 = L_3 / Decimal('2') + z_R_1 ** Decimal('2') * Decimal('2') / L_3
+
+res1 = rs.Resonator(3, rs.Mirror(Decimal('0'), Decimal('0'), r_1), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_2[0]), rs.Mirror(Decimal('2') * l_1 + L_3[0], Decimal('0'), r_3[0]))
+print(res1.waist_search(FUND_WAVE))
+na = np.zeros(L_3.size)
+for i in range(L_3.size):
+    res1 = rs.Resonator(3, rs.Mirror(Decimal('0'), Decimal('0'), r_1, in_plane_angle_deviation= C.power(Decimal('10'), Decimal('-13'))), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_2[i]), rs.Mirror(Decimal('2') * l_1 + L_3[i], Decimal('0'), r_3[i]))
+    na[i] = res1.realign()
+
+res2 = rs.Resonator(2, rs.Mirror(Decimal('0'), Decimal('0'), r_1, in_plane_angle_deviation= C.power(Decimal('10'), Decimal('-13'))), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_1))
+na2 = res2.realign()
+na = na2 / na
+plt.plot(L_3 / 2 / l_1, na)
 plt.show()
