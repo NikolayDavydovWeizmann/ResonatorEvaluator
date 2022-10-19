c8575336eb15e6c12b2cf38ac0af0961a61df82d
Nikolay Davydov <nikolay.davydov@weizmann.ac.il>
2022-10-19 10:18:24 +0300

    Mirror rot analisys

--- analitical_plot2.py
+++ analitical_plot2.py
@@ -13,13 +13,15 @@ FUND_WAVE = Decimal('1064') * C.power(Decimal('10'), Decimal('-7'))
 ANGLE = Decimal('15') / Decimal('180') * PI
 """
 
-NA = Decimal('0.2')
+NA = Decimal('0.1')
 z_R = FUND_WAVE / PI / C.power(NA, Decimal('2'))
 
 l_1 = Decimal('1')
-L_3 = np.arange(Decimal('2'), Decimal('1000'), Decimal('2'))
+L_3 = np.arange(Decimal('2'), Decimal('100'), Decimal('2'))
 
-f = (Decimal('1') + z_R ** Decimal('2')) / (Decimal('1') + ((Decimal('1') + z_R ** Decimal('2')) / L_3) + np.sqrt(((Decimal('1') + z_R ** Decimal('2')) / L_3)) ** Decimal('2') - z_R ** Decimal('2'))
+f = np.empty(L_3.size, dtype='O')
+for i in range(L_3.size):
+    f[i] = (Decimal('1') + z_R ** Decimal('2')) / (Decimal('1') + ((Decimal('1') + z_R ** Decimal('2')) / L_3[i]) + C.sqrt(((Decimal('1') + z_R ** Decimal('2')) / L_3[i]) ** Decimal('2') - z_R ** Decimal('2')))
 z_R_1 = z_R * (Decimal('1') + z_R ** Decimal('2')) / (((Decimal('1') + z_R ** Decimal('2')) / f - Decimal('1')) ** Decimal('2') + z_R ** Decimal('2'))
 
 r_1 = l_1 + z_R ** Decimal('2') / l_1
@@ -27,14 +29,19 @@ r_2 = Decimal('2') * f
 r_3 = L_3 / Decimal('2') + z_R_1 ** Decimal('2') * Decimal('2') / L_3
 
 res1 = rs.Resonator(3, rs.Mirror(Decimal('0'), Decimal('0'), r_1), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_2[0]), rs.Mirror(Decimal('2') * l_1 + L_3[0], Decimal('0'), r_3[0]))
+
+plt.show()
 print(res1.waist_search(FUND_WAVE))
-na = np.zeros(L_3.size)
+na = np.empty(L_3.size, dtype= 'O')
 for i in range(L_3.size):
     res1 = rs.Resonator(3, rs.Mirror(Decimal('0'), Decimal('0'), r_1, in_plane_angle_deviation= C.power(Decimal('10'), Decimal('-13'))), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_2[i]), rs.Mirror(Decimal('2') * l_1 + L_3[i], Decimal('0'), r_3[i]))
-    na[i] = res1.realign()
+    na_tmp = res1.waist_search(FUND_WAVE)[0, 0, 2]
+    res1.realign()
+    na[i] = res1.waist_search(FUND_WAVE)[0, 0, 2] / na_tmp - Decimal('1')
 
 res2 = rs.Resonator(2, rs.Mirror(Decimal('0'), Decimal('0'), r_1, in_plane_angle_deviation= C.power(Decimal('10'), Decimal('-13'))), rs.Mirror(Decimal('2') * l_1, Decimal('0'), r_1))
-na2 = res2.realign()
-na = na2 / na
-plt.plot(L_3 / 2 / l_1, na)
+na2_init = res2.waist_search(FUND_WAVE)[0, 0, 2]
+res2.realign()
+na2 = res2.waist_search(FUND_WAVE)[0, 0, 2] / na2_init - Decimal('1')
+plt.plot(L_3 / Decimal('2') / l_1, na / na2)
 plt.show()
